3ce665efdf15b6c0c469eca8989f33844784a2c7
Łukasz Obwarzanek <lobwarzanek@proaudio.local>
2017-07-26 15:43:33 +0200

    fix

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ var b = prompt('Podaj liczbę nr 2: ');
 var value = (a * a) + (2 * a * b) - (b - b);
 console.log(value);
 
-var score = value >= 0 ? 'Wynik dodatni' : 'Wynik ujemny';
-console.log(score);
-
-if (value === 0) {
-    console.log('Wynik to 0');
+if (value > 0) {
+    console.log('Wynik jest dodatni');
+} else if (value === 0) {
+    console.log('Wynik jest równy 0');
 } else {
-    console.log('Wynik to ' + value);
+    console.log('Wynik jest ujemny');
 }
-
-
